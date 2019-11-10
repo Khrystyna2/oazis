3519aba04c0726718cfe81fb3f657781616efea8
Khrystyna <l.xrustja@gmail.com>
2019-11-10 19:21:55 +0200

    added new pages

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,10 +1,34 @@
+$(window).on("load", function() {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		$("body").addClass("ios");
+	} else {
+		$("body").addClass("web");
+	}
+	$("body").removeClass("loaded");
+});
+
 $(document).ready(function() {
-	$(".open-phone-list").click(function() {
-		$(this).toggleClass("open");
-		$(this)
-			.next()
-			.fadeToggle(400);
+	// fixed menu
+	let heightHeader = $(".header").height();
+	let heightMenu = $(".header__nav").height();
+
+	$(window).scroll(function() {
+		let currentPosition = $(window).scrollTop();
+		if (currentPosition >= heightHeader - heightMenu) {
+			$(".header__nav").addClass("fixed");
+			$(".header").css("padding-bottom", heightMenu);
+		} else {
+			$(".header__nav").removeClass("fixed");
+			$(".header").css("padding-bottom", "0");
+		}
 	});
+
+	// $(".open-phone-list").click(function() {
+	// 	$(this).toggleClass("open");
+	// 	$(this)
+	// 		.next()
+	// 		.fadeToggle(400);
+	// });
 
 	// open language
 	$(".language>li>a").click(function() {
@@ -35,19 +59,32 @@ $(document).ready(function() {
 	let scrollLink = $(".scroll-section");
 	scrollLink.click(function(e) {
 		e.preventDefault();
-		scrollLink.removeClass("active");
-		$(this).addClass("active");
 		$("body,html").animate(
 			{
-				scrollTop: $(this.hash).offset().top
+				scrollTop:
+					$(this.hash)
+						.find(".scroll-to")
+						.offset().top - 120
 			},
 			1000
 		);
 	});
 
-	// if ($("select").length === 1) {
-	// 	$("select").styler();
-	// }
+	$(window).scroll(function() {
+		let scrollbarLocation = $(this).scrollTop();
+		scrollLink.each(function() {
+			let sectionOffset = $(this.hash).offset().top;
+			if (sectionOffset <= scrollbarLocation) {
+				$(this)
+					.parent()
+					.addClass("active");
+				$(this)
+					.parent()
+					.siblings()
+					.removeClass("active");
+			}
+		});
+	});
 
 	// tabs
 	$(".tab-navigation").each(function() {
@@ -74,10 +111,9 @@ $(document).ready(function() {
 
 	// slider car
 	$(".car__slider").slick({
-		infinite: false,
+		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1
-		// fade: true
 	});
 
 	$(".tab-navigation__item").click(function() {
@@ -169,5 +205,93 @@ $(document).ready(function() {
 		e.stopPropagation();
 	});
 
+	// slider gallery team
+	$(function() {
+		$(".slider").slick({
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			adaptiveHeight: true
+		});
+
+		$(".open-pop-up").click(function() {
+			if ($("#galleryTeam").hasClass("show")) {
+				$(".slider").resize();
+			}
+		});
+	});
+
 	$(".tel").mask("(999) 999-9999");
+
+	new WOW().init();
+
+	// animate car on scroll
+	const tween = new TimelineLite();
+
+	tween
+		.add(
+			TweenLite.to("#carMoving", 1, {
+				x: "200%",
+				ease: Power1.easeInOut
+			})
+		)
+		.add(TweenLite.to(".wheel", 1, { rotation: "+=360" }), "-=1");
+
+	const controller = new ScrollMagic.Controller();
+
+	const scene = new ScrollMagic.Scene({
+		triggerElement: ".schedule",
+		triggerHook: 0,
+		duration: 2400
+	})
+		.setTween(tween)
+		.addTo(controller);
+
+	$(".open-info").click(function() {
+		$(this)
+			.next(".social-package__info")
+			.fadeIn();
+	});
+
+	$(".close-info").click(function() {
+		$(this)
+			.parent()
+			.fadeOut();
+	});
+
+	// animation dots on map first screen
+	[1, 2, 3, 4, 5].forEach(i => {
+		const motionPath = MorphSVGPlugin.pathDataToBezier(`#path${i}`, { align: `#dots${i}` });
+		const tl = new TimelineMax({ repeat: -1 });
+		TweenLite.set(`#dots${i}`, { xPercent: -50, yPercent: -50 });
+		tl.to(`#dots${i}`, 160, { bezier: { values: motionPath, type: "cubic", ease: Linear.easeNone, repeatDelay: 0 } });
+	});
+
+	$(".car__gallery-link").click(function() {
+		$("body").addClass("overflow-hidden");
+	});
+	$(".lb-close, .lightboxOverlay").click(function() {
+		$("body").removeClass("overflow-hidden");
+	});
 });
+
+if ($("*").is(".cookie")) {
+	$(document).ready(function() {
+		$(".cookie")
+			.fadeIn(400)
+			.addClass("show");
+		$("body").addClass("overflow-hidden");
+		// close cookie
+		$(".cookie__close, .cookie__content").click(function(e) {
+			e.preventDefault();
+			$(".cookie")
+				.fadeOut(400)
+				.removeClass("show");
+			$("body").removeClass("overflow-hidden");
+		});
+
+		$(".cookie__body").click(function(e) {
+			e.stopPropagation();
+		});
+	});
+}
